bba7e78e2212b24f437a65114c8277d506b50e2e
Efe İ <41235050+EchTR@users.noreply.github.com>
2020-03-16 23:08:48 +0300

    Add files via upload

---
+++ files/main.py
@@ -0,0 +1,54 @@
+# Github: EchTR
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+class txtdb:
+	def __init__(self,file_name,var_name="none",var_value="none"):
+		self.file_name = ("{}.txt".format(file_name))
+		self.var_name = var_name
+		self.var_value = var_value
+	def variables(self):
+		f_file = open(self.file_name,"r")
+		que2 = 0
+		variables = 0
+		total_l = 0
+		l_l = 0
+		variable_list = {}
+		for i in f_file:
+			total_l = total_l + 1
+		f_file.seek(0)
+		for i in f_file:
+			l_l = l_l + 1
+			if l_l != total_l:
+				if ">" in i:
+					for k in i:
+						if k == ">":
+							variable_list.update({str(i[0:que2-1]):str(i[que2+2:len(i)-1])})
+							variables = variables + 1
+						que2 = que2 + 1
+			else:
+				if ">" in i:
+					for k in i:
+						if k == ">":
+							variable_list.update({str(i[0:que2-1]):str(i[que2+2:len(i)])})
+							variables = variables + 1
+						que2 = que2 + 1
+			que2 = 0
+		return variable_list	
+	def numofvars(self):
+		f_file = open(self.file_name,"r")
+		que2 = 0
+		variables = 0
+		for i in f_file:
+			if ">" in i:
+				for k in i:
+					if k == ">":
+						variables = variables + 1
+					que2 = que2 + 1
+			que2 = 0
+		return variables	
+	def append_var(self):
+		f_file = open(self.file_name,"a+")
+		f_file.write("\n{} > {}".format(self.var_name,self.var_value))
+		f_file.close()
+print(txtdb("veritabani","isim","efe").append_var())				
+input("")
